882045425c084082f6d5deed54c8408e09b9b680
CharafChroudi <charafchroudi17@gmail.com>
2020-03-13 10:19:24 +0100

    Solved questions

--- warmUp1.js
+++ warmUp1.js
@@ -3,4 +3,21 @@
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
 // 4-calculate your age in seconds.
 
-// your code is here
+
+function merge(first , last){
+	return first + ' ' + last ;
+}
+function multiple(){
+	if (13 % 3 === 0){
+		return "the number 13 is a multiple of 3";
+	}
+	return "the number 13 is not a multiple of 3";
+}
+
+function average(){
+	return	(13 + 14 + 13 + 15 + 16 + 17 + 19 + 13 + 16 + 15) / 10;
+}
+
+function age(){
+	return 23 * 356 * 24 * 3600 ;
+}
