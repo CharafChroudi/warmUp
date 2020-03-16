3e0634c31b6066947fc3096791698c2e7075a1d8
CharafChroudi <charafchroudi17@gmail.com>
2020-03-16 09:58:58 +0100

    Solved warmUp3

--- warmUp3.js
+++ warmUp3.js
@@ -1,13 +1,23 @@
 // 1 - Write functions larger and smaller that each accept two strings as arguments, and return the larger and smaller strings, respectively
+function larger(string1, string2){
+	if(string1.length > string2.length){
+		return string1;
+	}
+		return string2;
+}
 
+function smaller(string1, string2){
+	if(string1.length > string2.length){
+		return string2;
+	}
+		return string1;
+}
 // 2 - Write a function called counter that, when invoked, always returns a number that is one more than the previous invocation. For instance:
-// function counting(n) { 
-    TODO: your code here 
-// } 
-// counting(5); // => '1, 2, 3, 4, 5' 
-// counting(1); // => '1' 
-// counting(3); // => '1, 2, 3'
-
+    function counter(){
+    var	counter = 0;
+    counter = counter + 1;
+    return counter + counter();
+}
 
 // 3 -  Meet & Greet: n numbers are coming to a party for numbers. Introduce them each to all the guests as they arrive. You should end up with output looking something like this one for n = 5
 // welcome 1
@@ -15,7 +25,14 @@
 //  welcome 3, meet 1 and 2
 //  welcome 4, meet 1, 2 and 3
 //  welcome 5, meet 1, 2, 3 and 4
-//
+function meetGreet(n){
+	if (n <= 1){
+		return 'Welcome ' + n;
+	}
+		return 'Welcome ' + meetGreet(n + 1) + ', meet ' + meetGreet(n - 1);
+}
 
 // 4- Write a function shortestWord that works like longestWord, but returns the shortest word instead.
+function shortestWord(){
 
+}
