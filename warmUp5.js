fb4da6672122ebd5cf7375c6e137df583090c5e3
CharafChroudi <charafchroudi17@gmail.com>
2020-03-18 10:12:09 +0100

    Solved WarmUp 5

--- warmUp5.js
+++ warmUp5.js
@@ -2,12 +2,28 @@
 // - takes 2 arguments, both numbers.
 // - returns whichever number is the greater (higher) number.
 // ex greaterNum(5, 10) => "The greater number of 5 and 10 is 10."
-
+function greaterNum(a, b){
+	if (a > b){
+		return "The greater number of " + a + " and " + b + " is " + a;
+	}
+		return "The greater number of " + a + " and " + b + " is " + b;
+	}
 // 2) Write a function named isEven using  for loop that
 // -  iterate from x to y.
 // -  return array contain the even values,
 //  ex:  isEven(1,10) => [2,4,6,8,10]
-
+function isEven(x, y){
+	var even = [];
+	for (var i = 0; i < even.length; i++){
+		while (x <= y) {
+			if (x % 2 === 0) {
+				even[i] = even.push(x)
+			}
+			x++
+		}
+	}
+	return even;
+}
 // function isEven(x, y) {
 //     var arr = [];
 //     for (var i = 0; i < arr.length; i++) {
@@ -23,12 +39,30 @@
 //3) write a function named sum that
 // - Use a while loop to add up the numbers from x to y.
 // ex sum(1,5) => 15
+function sum(x, y){
+	var result = 0;
+	while (x <=y){
+		result = result + x;
+		x++;
+	}
+	return result;
+}
 
 //4) Write a function named factorial that
 // -  Use Recursion to calculate the factorial of a number
 // -  the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n
 // - 5! = 5*4*3*2*1 = 120
 // ex : factorial(5) => 120
+
+function factorial(n) {
+	if (n === 1 || n === 0) {
+			return 1;
+	}else if (n > 1){
+			return n * factorial(n - 1);
+	}else{
+			return -(n * factorial(n + 1));
+	}	
+}
 
 //5) write a function named decimals
 //- the function will format a number up to specified decimal places
@@ -41,3 +75,6 @@
 //      decimals(2.100212, 3) ==> '2.100'
 //      decimals(2100, 2) ==> '2100.00'
 //
+function decimals(n,decimal){
+	
+}
