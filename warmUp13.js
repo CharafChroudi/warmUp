94671a9ca2a2e192fd67e5a853ea57a6f49b5b47
CharafChroudi <charafchroudi17@gmail.com>
2020-03-27 09:51:22 +0100

    Solved WarmUp 13

--- warmUp13.js
+++ warmUp13.js
@@ -5,8 +5,26 @@
 //starting from 0, e.g.:
 // mult(3); // => 6
 // mult(4); // => 24
-
+//So I will be using recursion to build this function to keep reoccuring the same action until we reach the stop condition.
+function mult(n){
+	var total = 0;
+	if(n <= 1){
+		return 1;
+	}else if(n > 1){
+		return total = n * mult(n - 1);
+	}
+}
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number.
 //Have it return the new string.
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+//I create an empty string to start adding the incremenation twice until it reaches n then I slice the first element.
+function sameNum(n){
+	var string = '';
+	var i = 1;
+	while (n >= i){
+		string = string + ' ' + i + ' ' + i ;
+		i++;
+	}
+	return string.slice(2);
+}
