5661bc68fff44ebec4f352bf5e827cced90b1190
CharafChroudi <charafchroudi17@gmail.com>
2020-03-28 09:46:19 +0100

    Solved warmUp 14

--- warmUp14.js
+++ warmUp14.js
@@ -7,3 +7,22 @@
 // array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 // array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 // array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+//I will compare the two arrays together position by position if it matchs add it to the empty array if it doesn't increment and move to the next. then if the length of the new array matchs the sub then return true.
+function isSubset(main, sub){
+ 	var matchArr = [];
+ 	var i = 0;
+ 	var n = 0;
+ 	while(i < main.length){
+		if(main[i] === sub[n]){
+ 		matchArr.push(sub[n]);
+ 		i = 0;
+ 		n++;
+ 		}else if(main[i] !== sub[n]){
+			i++;
+ 		}
+ 	}
+ 	if(matchArr.length === sub.length){
+ 		return true;
+ 	}
+ 		return false;
+ }
